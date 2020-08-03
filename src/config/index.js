a285ea52a0e61341d3478b8f9a6209aa5952b015
Davi Souza <davisouza1997jf@gmail.com>
2020-08-03 20:04:04 -0300

    trocando a requisição pra HTTPS

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACK = window.location.href.includes('localhost')
   ? 'http://localhost:8080/'
-  : 'http://davi-flix.herokuapp.com/';
+  : 'https://davi-flix.herokuapp.com/';
 
 export default {
   URL_BACK,
